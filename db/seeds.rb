9e64cdabcf0e849c78ca37c3a49c008c368e79d0
Daryl Rogony <darylrogony@gmail.com>
2022-10-03 11:27:55 +0300

    FIt: create the data to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Pizza.create!(name: 'veggie pizza', ingredients: 'mushrooms and peppers')
+Pizza.create!(name: 'Pepperoni Pizza', ingredients: 'beef and pork')
+Pizza.create!(name: 'BBQ Chicken pizza', ingredients: 'bbq sauce, mozzarella, chicken')
+Pizza.create!(name: 'Cheese pizza', ingredients: 'fontina, cheddar, provolone')
+
+Restaurant.create!(name: 'McDonalds', address: '41180-7830' )
+Restaurant.create!(name: 'Starbucks', address: '3717 Keyser Ridge Road' )
+Restaurant.create!(name: 'Taco Bell', address: '3625 Science Center Drive' )
+Restaurant.create!(name: 'Pizza Hut', address: '1721 Patterson Fork Road' )

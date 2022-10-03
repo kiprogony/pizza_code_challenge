9d09f767b7e14b945612507938c620a049ddfa2c
Daryl Rogony <darylrogony@gmail.com>
2022-10-03 11:27:07 +0300

    Fit: add routes actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,17 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    restaurant = Restaurant.all
+    render json: restaurant
+  end
+
+  def show
+    restaurant = Restaurant.find_by(id: params[:id])
+    if restaurant
+      render json: restaurant
+    else
+      render json: { error: "Restaurant not found"}, status: :not_found
+    end
+
+  end
 end
